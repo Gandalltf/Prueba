88464f5853a2ca832f8882e02be0466cfd430672
Gandalltf <juanjopb93@icloud.com>
2023-02-22 22:12:06 +0100

    cambios de prueba

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import CountdownTimer from './CountdownTimer';
 
 export default function App() {
   const timestamp = 1708772400000
-  alert (new Date(1708772400000))
+  //alert (new Date(1708772400000))
 
   return (
     <div>
-      <h1>Enlace Juanfran & Maria Dolores</h1>
+      <h1>Enlace Juanfran & Maria Dolores Guapis Os quiero </h1>
 
       <h2>24 - Febrero - 2024</h2>
       <CountdownTimer targetDate={timestamp}/>
